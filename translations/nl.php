de1521e61ea38e24fc4e4ab979b397864cfd127c
Thijs Kaspers <thijs@24creative.nl>
2015-10-27 15:40:02 +0100

    Dutch translations

---
+++ translations/nl.php
@@ -0,0 +1,17 @@
+<?php
+
+// Dutch translations
+array(
+    'Email Address' => 'E-mailadres',
+    'Phone Number' => 'Telefoonnummer',
+    'Custom URL' => 'Handmatige URL',
+    'Entry' => 'Pagina',
+    'Category' => 'Categorie',
+    'Asset' => 'Bestand',
+    'Please enter a valid email address.' => 'Vul a.u.b. een geldig e-mailadres in.',
+    'Please enter a valid telephone.' => 'Vul a.u.b. een geldig telefoonnummer in.',
+    'Please enter a valid url.' => 'Vul a.u.b. een geldige URL in.',
+    'Please select an entry.' => 'Selecteer een pagina.',
+    'Please select an asset.' => 'Selecteer een bestand.',
+    'Please select a category.' => 'Selecteer een categorie.',
+);
